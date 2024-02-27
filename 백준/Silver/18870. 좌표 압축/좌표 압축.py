ba38c0f940b0308565924ccd5174fe114d29d10c
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-02-28 01:15:59 +0900

    [Silver II] Title: 좌표 압축, Time: 2368 ms, Memory: 218508 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+nums = [0] * N
+
+arr_index = [(index, value) for index, value in enumerate(arr)]
+
+arr_index.sort(key=lambda x: x[1])
+
+nums[arr_index[0][0]] = 0
+
+current_idx = 0
+for i in range(1, N):
+    if arr_index[i][1] != arr_index[i - 1][1]:
+        current_idx += 1
+    nums[arr_index[i][0]] = current_idx
+
+print(*nums)
