2c5a5d03fe096f546b8227a98a564616b15ace0c
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-03-01 23:04:44 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 488 ms, Memory: 253044 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -0,0 +1,11 @@
+S = input().rstrip()
+
+result = []
+
+length = len(S)
+
+for start in range(length):
+    for end in range(start + 1, length + 1):
+        result.append(S[start:end])
+
+print(len(set(result)))
