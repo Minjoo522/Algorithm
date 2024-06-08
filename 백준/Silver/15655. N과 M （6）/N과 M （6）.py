04a9f3d75f8e2c1b03566955928d209a72cc646a
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-06-08 22:50:26 +0900

    [Silver III] Title: N과 M (6), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -2,22 +2,22 @@ import sys
 
 input = sys.stdin.readline
 
+N, M = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+sel = [0] * M
+
 
 def combination(idx, sidx):
     if sidx == M:
-        print(*ans)
+        print(*sel)
         return
 
     if idx == N:
         return
 
-    ans[sidx] = nums[idx]
+    sel[sidx] = nums[idx]
     combination(idx + 1, sidx + 1)
     combination(idx + 1, sidx)
 
-
-N, M = map(int, input().split())
-nums = sorted(list(map(int, input().split())))
-ans = [0] * M
 
 combination(0, 0)
