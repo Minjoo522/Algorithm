1fbbd62519018d6d4e757ea23c16a8ced2bf9e74
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-07-08 12:48:51 +0900

    [Gold V] Title: 내려가기, Time: 324 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+pre = list(map(int, input().split()))
+max_pre = pre[:]
+min_pre = pre[:]
+
+for _ in range(n - 1):
+    aft = list(map(int, input().split()))
+
+    max_aft = [aft[0] + max(max_pre[0], max_pre[1]), aft[1] + max(max_pre[0], max_pre[1], max_pre[2]), aft[2] + max(max_pre[1], max_pre[2])]
+    min_aft = [aft[0] + min(min_pre[0], min_pre[1]), aft[1] + min(min_pre[0], min_pre[1], min_pre[2]), aft[2] + min(min_pre[1], min_pre[2])]
+
+    max_pre = max_aft
+    min_pre = min_aft
+
+print(max(max_pre), min(min_pre))
