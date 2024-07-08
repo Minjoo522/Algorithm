6ce3218a4cc6d62d292d611d3a13c9be9a502618
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-07-08 12:55:11 +0900

    [Gold V] Title: 내려가기, Time: 316 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -10,10 +10,15 @@ min_pre = pre[:]
 for _ in range(n - 1):
     aft = list(map(int, input().split()))
 
-    max_aft = [aft[0] + max(max_pre[0], max_pre[1]), aft[1] + max(max_pre[0], max_pre[1], max_pre[2]), aft[2] + max(max_pre[1], max_pre[2])]
-    min_aft = [aft[0] + min(min_pre[0], min_pre[1]), aft[1] + min(min_pre[0], min_pre[1], min_pre[2]), aft[2] + min(min_pre[1], min_pre[2])]
-
-    max_pre = max_aft
-    min_pre = min_aft
+    max_pre = [
+        aft[0] + max(max_pre[0], max_pre[1]),
+        aft[1] + max(max_pre[0], max_pre[1], max_pre[2]),
+        aft[2] + max(max_pre[1], max_pre[2])
+    ]
+    min_pre = [
+        aft[0] + min(min_pre[0], min_pre[1]),
+        aft[1] + min(min_pre[0], min_pre[1], min_pre[2]),
+        aft[2] + min(min_pre[1], min_pre[2])
+    ]
 
 print(max(max_pre), min(min_pre))
