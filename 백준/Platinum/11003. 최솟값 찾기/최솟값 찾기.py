6332d946c8739ac121ed01d87d5dc271bcdc1e21
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-07-09 10:21:07 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 7648 ms, Memory: 757400 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -3,14 +3,14 @@ from collections import deque
 
 input = sys.stdin.readline
 
-n, l = map(int, input().split())
+N, L = map(int, input().split())
 mydeque = deque()
 now = list(map(int, input().split()))
 
-for i in range(n):
+for i in range(N):
     while mydeque and mydeque[-1][0] > now[i]:
         mydeque.pop()
     mydeque.append((now[i], i))
-    if mydeque[0][1] <= i - l:
+    if mydeque[0][1] <= i - L:  # 윈도우 범위 넘어감 now index - L <= index
         mydeque.popleft()
     print(mydeque[0][0], end=' ')
