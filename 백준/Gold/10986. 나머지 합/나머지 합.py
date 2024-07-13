94a91e82e88049f3534480150ea5de4a3901311c
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-07-13 16:46:48 +0900

    [Gold III] Title: 나머지 합, Time: 712 ms, Memory: 142240 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -2,24 +2,26 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-A = list(map(int, input().split()))
-S = [0] * n
-C = [0] * m  # 업데이트 된 합 배열 중 동일한 값을 가진 애들이 몇 개 인지
-answer = 0
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+S = [0] * N  # 합 배열
+C = [0] * M  # 업데이트 된 합 배열 중 동일한 값을 가진 애들이 몇 개인지 저장하는 배열
+ans = 0
 
-S[0] = A[0]
-for i in range(1, n):
-    S[i] = S[i - 1] + A[i]
+S[0] = nums[0]
+# 누적 합
+for i in range(1, N):
+    S[i] = nums[i] + S[i - 1]
 
-for i in range(n):
-    remainder = S[i] % m
+# 합 배열 업데이트
+for i in range(N):
+    remainder = S[i] % M
     if remainder == 0:
-        answer += 1
+        ans += 1
     C[remainder] += 1
 
-for i in range(m):
+for i in range(M):
     if C[i] > 1:  # 같은 나머지 값을 갖는 애가 2개 이상 있으면
-        answer += C[i] * (C[i] - 1) // 2  # combination 공식(무조건 2개 뽑음)
+        ans += C[i] * (C[i] - 1) // 2  # combiantion 공식 (무조건 2개)
 
-print(answer)
+print(ans)
