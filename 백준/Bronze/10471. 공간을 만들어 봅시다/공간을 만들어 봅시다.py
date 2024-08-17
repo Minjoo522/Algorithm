4aeeca56f08deefe39f36531d73efb2a659e35e5
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-08-18 00:33:21 +0900

    [Bronze II] Title: 공간을 만들어 봅시다, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10471. 공간을 만들어 봅시다/공간을 만들어 봅시다.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+W, P = map(int, input().split())
+partitions = [0] + list(map(int, input().split())) + [W]
+
+sizes = set()
+
+for i in range(0, len(partitions) - 1):
+    for j in range(i + 1, len(partitions)):
+        sizes.add(partitions[j] - partitions[i])
+
+print(*sorted(list(sizes)))
