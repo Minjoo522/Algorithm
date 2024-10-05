511bc7264d75715f59eef3435c6f4b44106c1ccf
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-05 22:21:08 +0900

    [level 0] Title: 더 크게 합치기, Time: 10.49 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,9 +1,7 @@
 class Solution {
     public int solution(int a, int b) {
-        String a_str = String.valueOf(a);
-        String b_str = String.valueOf(b);
-        int ab = Integer.parseInt(a_str + b_str);
-        int ba = Integer.parseInt(b_str + a_str);
+        int ab = Integer.parseInt("" + a + b);
+        int ba = Integer.parseInt("" + b + a);
         return ab > ba ? ab : ba;
     }
 }
