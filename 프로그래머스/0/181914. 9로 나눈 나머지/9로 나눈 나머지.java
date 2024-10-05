427ae88c79ec626da9ed5afec965b3f5916f93fb
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-06 02:30:48 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 46.47 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String number) {
+        int sum = 0;
+        for (String num: number.split("")) {
+            sum += Integer.parseInt(num);
+        }
+        return sum % 9;
+    }
+}
