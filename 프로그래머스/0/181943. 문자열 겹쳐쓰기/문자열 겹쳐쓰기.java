91e4cf471ef39f379577c8cf94c9f29909fd8763
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-05 22:09:10 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 19.58 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,11 +1,7 @@
-import java.lang.StringBuffer;
-
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        StringBuffer sb = new StringBuffer();
-        sb.append(my_string.substring(0, s));
-        sb.append(overwrite_string);
-        sb.append(my_string.substring(s + overwrite_string.length(), my_string.length()));
-        return sb.toString();
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
+        return before + overwrite_string + after;
     }
 }
