4f3f565b82f2ff413d94811fca77a30696cebbec
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-05 23:23:28 +0900

    [level 0] Title: 코드 처리하기, Time: 10.08 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -4,22 +4,16 @@ import java.lang.StringBuffer;
 class Solution {
     public String solution(String code) {
         StringBuffer sb = new StringBuffer();
-        List<Integer> modes = List.of(0, 1);
-        int currentMode = 0;
+        int mode = 0;
         
         for (int i = 0; i < code.length(); i++) {
             char ch = code.charAt(i);
             if (ch == '1') {
-                currentMode = modes.get((currentMode + 1) % 2);
+                mode = mode == 0 ? 1 : 0;
                 continue;
             }
-            if (currentMode == 0 && i % 2 == 0) {
+            if (i % 2 == mode) {
                 sb.append(ch);
-                continue;
-            }
-            if (currentMode == 1 && i % 2 != 0) {
-                sb.append(ch);
-                continue;
             }
         }
         String answer = sb.toString();
