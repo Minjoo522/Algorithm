92d6bcc6e36142146fb6fd12ec4be21e9f4da477
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-05 21:48:52 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 196.93 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = a + b;
+
+        System.out.println(String.format("%d + %d = %d", a, b, result));
+    }
+}
