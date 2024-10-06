e9624947181e5d56cbcf92824f78fed32ad9c68c
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-07 00:14:01 +0900

    [Silver V] Title: 너의 수능 점수가 궁금해, Time: 128 ms, Memory: 16264 KB -BaekjoonHub

---
+++ 백준/Silver/29808. 너의 수능 점수가 궁금해/너의 수능 점수가 궁금해.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int studentNum = Integer.parseInt(input);
+
+        if (studentNum % 4763 > 0) {
+            System.out.println(0);
+            System.exit(0);
+        }
+
+        int tmp = studentNum / 4763;
+
+        List<List<Integer>> results = new ArrayList<>();
+        for (int i = 0; i <= 200; i++) {
+            for (int j = 0; j <= 200; j++) {
+                if (
+                        tmp == i * 508 + j * 212 ||
+                        tmp == i * 508 + j * 305 ||
+                        tmp == i * 108 + j * 212 ||
+                        tmp == i * 108 + j * 305
+                        ) {
+                    results.add(List.of(i, j));
+                }
+            }
+        }
+
+        results.sort((a, b) -> {
+            if (!a.get(0).equals(b.get(0))) {
+                return a.get(0) - b.get(0);
+            } else {
+                return a.get(1) - b.get(1);
+            }
+        });
+
+        System.out.println(results.size());
+        for (List<Integer> list: results) {
+            System.out.println(list.get(0) + " " + list.get(1));
+        }
+    }
+}
