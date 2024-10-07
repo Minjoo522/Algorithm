4fb67d1972f2d73255deada873e58c5b0941844f
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-07 13:54:10 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 27.53 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -4,14 +4,24 @@ class Solution {
     public String solution(String my_string, int[][] queries) {
         String current = my_string;
         for (int[] query: queries) {
+            StringBuffer sb = new StringBuffer();
             int s = query[0];
             int e = query[1];
             
-            String prefix = current.substring(0, s);
-            String toReverse = new StringBuilder(current.substring(s, e + 1)).reverse().toString();
-            String suffix = current.substring(e + 1);
+            for (int i = 0; i < s; i++) {
+                System.out.println("i 1 = " + i);
+                sb.append(current.charAt(i));
+            }
             
-            current = prefix + toReverse + suffix;
+            for (int i = e; i >= s; i--) {
+                System.out.println("i = " + i);
+                sb.append(current.charAt(i));
+            }
+            
+            for (int i = s + 1; i < current.length(); i++) {
+                sb.append(current.charAt(i));
+            }
+            current = sb.toString();
         }
         return current;
     }
