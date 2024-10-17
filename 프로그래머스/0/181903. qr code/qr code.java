b50078fa040cee585bab5a801c64c10f9b5eb8e4
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-17 10:23:00 +0900

    [level 0] Title: qr code, Time: 0.18 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -3,10 +3,8 @@ import java.lang.*;
 class Solution {
     public String solution(int q, int r, String code) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < code.length(); i++) {
-            if (i % q == r) {
-                sb.append(code.charAt(i));
-            }
+        for (int i = r; i < code.length(); i += q) {
+            sb.append(code.charAt(i));
         }
         return sb.toString();
     }
