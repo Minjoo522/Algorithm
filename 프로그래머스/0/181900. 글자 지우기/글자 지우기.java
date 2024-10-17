5fcb5312a37454eeb93b395ae6a72b9413d9090a
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-17 11:02:28 +0900

    [level 0] Title: 글자 지우기, Time: 0.68 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,20 +1,9 @@
-import java.util.*;
-import java.lang.*;
-
 class Solution {
     public String solution(String my_string, int[] indices) {
-        Set<Integer> idxSet = new HashSet<>();
-        for (int index: indices) {
-            idxSet.add(index);
+        String[] result = my_string.split("");
+        for (int i = 0; i < indices.length; i++) {
+            result[indices[i]] = "";
         }
-        
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < my_string.length(); i++) {
-            if (!idxSet.contains(i)) {
-                result.append(my_string.charAt(i));
-            }
-        }
-        
-        return result.toString();
+        return String.join("", result);
     }
 }
