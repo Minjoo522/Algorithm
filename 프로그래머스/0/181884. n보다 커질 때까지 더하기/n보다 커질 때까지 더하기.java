8a84fbbd63ab73592a84e96a5d3ff4f76a3f9a01
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-21 00:45:01 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int result = 0;
+        
+        for (int num: numbers) {
+            result += num;
+            if (result > n) {
+                break;
+            }
+        }
+        return result;
+    }
+}
