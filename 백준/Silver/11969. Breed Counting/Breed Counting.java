ce327ce2fd22eb9bded9b641f759ecf7eb0fddd6
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-25 08:55:33 +0900

    [Silver III] Title: Breed Counting, Time: 732 ms, Memory: 79616 KB -BaekjoonHub

--- 백준/Silver/11969. Breed Counting/Breed Counting.java
+++ 백준/Silver/11969. Breed Counting/Breed Counting.java
@@ -5,14 +5,15 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> cows = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
 
+        int[] cows = new int[n];
         for (int i = 0; i < n; i++) {
-            cows.add(Integer.parseInt(br.readLine()) - 1);
+            cows[i] = Integer.parseInt(br.readLine()) - 1;
         }
 
         // comulative sum, two-dimensional array
@@ -23,10 +24,9 @@ public class Main {
             counts[i][1] = counts[i - 1][1];
             counts[i][2] = counts[i - 1][2];
 
-            counts[i][cows.get(i - 1)] += 1;
+            counts[i][cows[i - 1]] += 1;
         }
 
-        int[] result = new int[q];
         for (int i = 0; i < q; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
@@ -35,7 +35,10 @@ public class Main {
             int holsteins = counts[end][0] - counts[start - 1][0];
             int huernseys = counts[end][1] - counts[start - 1][1];
             int jerseys = counts[end][2] - counts[start - 1][2];
-            System.out.printf("%d %d %d\n", holsteins, huernseys, jerseys);
+
+            sb.append(holsteins).append(" ").append(huernseys).append(" ").append(jerseys).append(System.lineSeparator());
         }
+
+        System.out.println(sb);
     }
 }
