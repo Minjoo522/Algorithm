c04385e334f734b68e19d985e124f2ea079217ee
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-26 00:46:11 +0900

    [level 0] Title: 배열 비교하기, Time: 0.06 ms, Memory: 66.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            return arr1.length > arr2.length ? 1 : -1;
+        }
+        
+        int arr1Sum = 0;
+        int arr2Sum = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            arr1Sum += arr1[i];
+            arr2Sum += arr2[i];
+        }
+        
+        if (arr1Sum == arr2Sum) {
+            return 0;
+        }
+        return arr1Sum > arr2Sum ? 1 : -1;
+    }
+}
