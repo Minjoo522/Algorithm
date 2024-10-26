2a12e5eef62bb974d5a4aa8c2dae8439a745bb36
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-26 11:12:32 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.55 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -3,11 +3,6 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
-        int[] result = new int[num_list.length - 5];
-        int idx = 0;
-        for (int i = 5; i < num_list.length; i++) {
-            result[idx++] = num_list[i];
-        }
-        return result;
+        return Arrays.copyOfRange(num_list, 5, num_list.length);
     }
 }
