3202d7c089c3b64e65db0633e9213223c41bfae9
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-28 13:29:44 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(String num_str) {
         int answer = 0;
         for (char ch : num_str.toCharArray()) {
-            answer += Character.getNumericValue(ch);
+            answer += ch - 48;
         }
         return answer;
     }
