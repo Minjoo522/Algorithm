9c487362aebe5cc85f89bbbdad10a3eb9952a4c9
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-28 13:53:52 +0900

    [level 0] Title: 0 떼기, Time: 13.85 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,12 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        while (true) {
-            if (n_str.startsWith("0")) {
-                n_str = n_str.replaceFirst("0", "");
-            } else {
-                break;
-            }
-        }
-        return n_str;
+        return "" + Integer.parseInt(n_str);
     }
 }
