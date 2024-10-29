7f92d00463591f4b72dd517c235794f8205c4cb0
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-29 17:16:58 +0900

    [level 0] Title: l로 만들기, Time: 9.65 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (char ch: myString.toCharArray()) {
+            sb.append(ch < 'l' ? 'l' : ch);
+        }
+        return sb.toString();
+    }
+}
