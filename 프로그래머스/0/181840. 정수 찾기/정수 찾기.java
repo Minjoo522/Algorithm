d8bdccc0f05b87effaa98bf155b9d07c996a75e7
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-29 16:28:25 +0900

    [level 0] Title: 정수 찾기, Time: 0.67 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,8 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] num_list, int n) {
-        for (int num: num_list) {
-            if (num == n) {
+        Arrays.sort(num_list);
+        
+        int low = 0;
+        int high = num_list.length - 1;
+        int mid;
+        
+        while (low <= high) {
+            mid = (low + high) / 2;
+            
+            if (num_list[mid] == n) {
                 return 1;
+            } else if (num_list[mid] > n) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return 0;
