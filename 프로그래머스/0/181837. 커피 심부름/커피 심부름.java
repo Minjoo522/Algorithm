15c9d9a3c16ac55751a3a81d672830a60f748a37
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-29 16:49:34 +0900

    [level 0] Title: 커피 심부름, Time: 1.21 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -2,11 +2,7 @@ class Solution {
     public int solution(String[] order) {
         int result = 0;
         for (String ord: order) {
-            if (ord.contains("americano") || ord.equals("anything")) {
-                result += 4500;
-            } else {
-                result += 5000;
-            }
+            result += ord.contains("cafelatte") ? 5000 : 4500;
         }
         return result;
     }
