39d31d1e3003330aaaab76ccec21c6ba6eef8c95
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-11-03 00:01:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.99 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,25 +1,18 @@
 import java.util.*;
 
 class Solution {
-    private static final int[] dx = {0, 0, -1, 1};
-    private static final int[] dy = {-1, 1, 0, 0};
-    private static final Queue<int[]> q = new LinkedList<>();
-    private static boolean[][] visited;
-    
     public int solution(int[][] maps) {
-        return bfs(maps, 0, 0);
-    }
-    
-    private int bfs(int[][] maps, int startX, int startY) {
         int rows = maps.length;
         int cols = maps[0].length;
         
-        visited = new boolean[maps.length][maps[0].length];
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[rows][cols];
+        int[][] directions = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
         
-        q.add(new int[]{startX, startY, 1});
-        visited[startX][startY] = true;
+        q.add(new int[]{0, 0, 1});
+        visited[0][0] = true;
         
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             int[] current = q.poll();
             int x = current[0];
             int y = current[1];
@@ -29,13 +22,13 @@ class Solution {
                 return distance;
             }
             
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            for (int[] direction: directions) {
+                int nx = x + direction[0];
+                int ny = y + direction[1];
                 
-                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols && maps[nx][ny] != 0 && !visited[nx][ny]) {
+                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols && maps[nx][ny] == 1 && !visited[nx][ny]) {
                     visited[nx][ny] = true;
-                    q.add(new int[]{nx, ny, distance + 1});
+                    q.offer(new int[]{nx, ny, distance + 1});
                 }
             }
         }
