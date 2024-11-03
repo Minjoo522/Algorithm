fc5576121c0ccf4c66fcfd90f5fa8a820d3a2cb4
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-11-03 11:21:53 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,24 @@
 class Solution {
-    private boolean[] visited;
-    
     public int solution(int n, int[][] computers) {
-        visited = new boolean[n];
+        boolean[] visited = new boolean[n];
         int count = 0;
         
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                dfs(i, computers);
+                dfs(i, computers, visited);
                 count++;
             }
         }
+        
         return count;
     }
     
-    private void dfs(int computer, int[][] computers) {
+    private void dfs(int computer, int[][] computers, boolean[] visited) {
         visited[computer] = true;
         
         for (int i = 0; i < computers.length; i++) {
             if (computers[computer][i] == 1 && !visited[i]) {
-                dfs(i, computers);
+                dfs(i, computers, visited);
             }
         }
     }
