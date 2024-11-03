0de10d63535d5501cd81d87bc65ea5d883f95b1a
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-11-03 22:39:06 +0900

    [level 3] Title: 정수 삼각형, Time: 13.62 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -3,8 +3,8 @@ import java.lang.*;
 class Solution {
     public int solution(int[][] triangle) {
         int height = triangle.length;
-        int[][] dp = new int[height][];
         
+        int[][] dp = new int[height][];
         for (int i = 0; i < height; i++) {
             dp[i] = triangle[i].clone();
         }
@@ -14,6 +14,7 @@ class Solution {
                 dp[i][j] += Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
             }
         }
+        
         return dp[0][0];
     }
 }
