b9b26052670cb9231bdeab1d90f6582c5b5702ea
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2025-03-09 15:37:42 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.03 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -2,12 +2,8 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
         
-        for (int num: num_list) {
-            if (num % 2 == 0) {
-                answer[0]++;
-            } else {
-                answer[1]++;
-            }
+        for (int i = 0; i < num_list.length; i++) {
+            answer[num_list[i] % 2]++;
         }
         
         return answer;
