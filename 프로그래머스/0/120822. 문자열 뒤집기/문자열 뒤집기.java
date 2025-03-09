6dfe192da0b3da5e5d4d6cad0332ecb0865caccc
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2025-03-09 15:40:14 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = my_string.length() - 1; i >= 0; i--) {
-            sb.append(my_string.charAt(i));
-        }
-        
-        return sb.toString();
+        return new StringBuilder(my_string).reverse().toString();
     }
 }
