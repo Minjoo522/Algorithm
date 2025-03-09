0a82c3fd753cd9e90cbc1c3c98760f72cbbc3779
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2025-03-09 15:29:07 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.06 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (char ch: my_string.toCharArray()) {
+            if (ch != letter.charAt(0)) {
+                sb.append(ch);
+            }
+        }
+        
+        return sb.toString();
+    }
+}
